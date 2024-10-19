58bbf5b92a2925979fd6a0cd7c109acca43cf19c
Leon Higgin <admin@leonmichaelhiggin.co.uk>
2024-10-19 22:35:40 +0100

    Notes

--- Assets/Scripts/ProjectInitializer.cs
+++ Assets/Scripts/ProjectInitializer.cs
@@ -1,6 +1,10 @@
 using GridSystem;
 using UnityEngine;
 
+/// <summary>
+/// Using a project initializer can help reduce the amount of race conditions by offering more granular control of the initialisation sequence.
+/// Also offers an exy way to implement dependency injections instead of always relying on the singleton pattern.
+/// </summary>
 public class ProjectInitializer : MonoBehaviour
 {
     private void Start()
